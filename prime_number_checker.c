e19c2010f9ebfb50ea8de8ed7338c28e58e7a41e
[sergio eneide] <[sergio.eneide@protonmail.com]>
2024-09-19 11:29:40 +0100

    second commit

---
+++ prime_number_checker.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int num;
+
+    // Integer input from user
+    printf("Enter a positive integer: ");
+    // Read value of user integer input, store in variable num
+    scanf("%d", &num);
+
+    
+}
