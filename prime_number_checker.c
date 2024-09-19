11dc2332bc83466205f6755c01aa74d9e762cf93
[sergio eneide] <[sergio.eneide@protonmail.com]>
2024-09-19 11:33:57 +0100

    fourth commit

--- prime_number_checker.c
+++ prime_number_checker.c
@@ -10,7 +10,7 @@ int main()
     // Read value of user integer input, store in variable num
     scanf("%d", &num);
 
-
+    // IF statement checks integer input from user is prime
     if (num > 1 && (num == 2 || num == 3 || num == 5 || num == 7 ||
        (num % 2 != 0 && num % 3 != 0 && num % 5 != 0 && num % 7 != 0)))
     {
